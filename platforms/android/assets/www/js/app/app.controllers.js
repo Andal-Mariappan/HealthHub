26c86b4509fac26b7efd9de15b1027bab766d818
secretmoo35 <sarayut-moo.th@hotmail.com>
2016-04-27 13:03:27 +0700

    feed

--- platforms/android/assets/www/js/app/app.controllers.js
+++ platforms/android/assets/www/js/app/app.controllers.js
@@ -153,11 +153,27 @@ angular.module('your_app_name.app.controllers', [])
 })
 
 
-.controller('FeedCtrl', function($scope, PostService) {
+.controller('FeedCtrl', function($scope, PostService,OpenFB) {
     $scope.posts = [];
     $scope.page = 1;
     $scope.totalPages = 1;
     $scope.chat2 = "gdfgdfg";
+
+    $scope.loadFeed = function() {
+        var pageID = 256097964403278
+        OpenFB.get('/' + pageID + '/posts?fields=full_picture,message,updated_time,from,picture', { limit: 20 })
+            .success(function(result) {
+                $scope.items = result.data;;
+
+                // Used with pull-to-refresh
+                $scope.$broadcast('scroll.refreshComplete');
+            })
+            .error(function(data) {
+                $scope.hide();
+                alert(data.error.message);
+            });
+    }
+
     $scope.doRefresh = function() {
         PostService.getFeed(1)
             .then(function(data) {
@@ -167,7 +183,6 @@ angular.module('your_app_name.app.controllers', [])
                 $scope.$broadcast('scroll.refreshComplete');
             });
     };
-
 
     $scope.getNewData = function() {
         //do something to load your new data here
@@ -270,7 +285,19 @@ angular.module('your_app_name.app.controllers', [])
     //$scope.paymentDetails;
 })
 
-.controller('SettingsCtrl', function($scope, $ionicModal) {
+.controller('SettingsCtrl', function($scope, $ionicModal, OpenFB, $state) {
+
+    $scope.signOut = function() {
+
+        OpenFB.revokePermissions().then(
+            function() {
+                $state.go('facebook-sign-in');
+            },
+            function() {
+                alert('OpenFB : Revoke Permissions Failed!ppppp');
+            });
+
+    }
 
     $ionicModal.fromTemplateUrl('views/app/legal/terms-of-service.html', {
         scope: $scope,
--- www/js/app/app.services.js
+++ www/js/app/app.services.js
@@ -1,179 +1,278 @@
 angular.module('your_app_name.app.services', [])
 
-.service('AuthService', function (){
+.service('AuthService', function() {
 
-  this.saveUser = function(user){
-    window.localStorage.your_app_name_user = JSON.stringify(user);
-  };
+    this.saveUser = function(user) {
+        window.localStorage.your_app_name_user = JSON.stringify(user);
+    };
 
-  this.getLoggedUser = function(){
+    this.getLoggedUser = function() {
 
-    return (window.localStorage.your_app_name_user) ?
-      JSON.parse(window.localStorage.your_app_name_user) : null;
-  };
+        return (window.localStorage.your_app_name_user) ?
+            JSON.parse(window.localStorage.your_app_name_user) : null;
+    };
 
 })
 
-.service('PostService', function ($http, $q){
+.service('PostService', function($http, $q,OpenFB ) {
 
-  this.getUserDetails = function(userId){
-    var dfd = $q.defer();
+    this.getUserDetails = function(userId) {
+        var dfd = $q.defer();
 
-    $http.get('database.json').success(function(database) {
-      //find the user
-      var user = _.find(database.users, function(user){ return user._id == userId; });
-      dfd.resolve(user);
-    });
+        $http.get('database.json').success(function(database) {
+            //find the user
+            var user = _.find(database.users, function(user) {
+                return user._id == userId;
+            });
+            dfd.resolve(user);
+        });
 
-    return dfd.promise;
-  };
+        return dfd.promise;
+    };
 
-  this.getPost = function(postId){
-    var dfd = $q.defer();
-    $http.get('database.json').success(function(database) {
-      var post = _.find(database.posts, function(posts){ return posts.id == postId; });
-      post.user = _.find(database.users, function(user){ return user._id == post.userId; });
-      for (var i =  post.feed_comment.length - 1; i >= 0; i--) {
-        post.feed_comment[i].user = _.find(database.users, function(user){ return user._id == post.feed_comment[i].userId; });
-      };
-      dfd.resolve(post);
-    });
-    return dfd.promise;
-  };
+    this.getPost = function(postId) {
+        var dfd = $q.defer();
+        $http.get('database.json').success(function(database) {
+            var post = _.find(database.posts, function(posts) {
+                return posts.id == postId;
+            });
+            post.user = _.find(database.users, function(user) {
+                return user._id == post.userId;
+            });
+            for (var i = post.feed_comment.length - 1; i >= 0; i--) {
+                post.feed_comment[i].user = _.find(database.users, function(user) {
+                    return user._id == post.feed_comment[i].userId;
+                });
+            };
+            dfd.resolve(post);
+        });
+        return dfd.promise;
+    };
 
-  this.getUserPosts = function(userId){
-    var dfd = $q.defer();
+    this.getUserPosts = function(userId) {
+        var dfd = $q.defer();
 
-    $http.get('database.json').success(function(database) {
+        $http.get('database.json').success(function(database) {
 
-      //get user posts
-      var userPosts =  _.filter(database.posts, function(post){ return post.userId == userId; });
-      //sort posts by published date
-      var sorted_posts = _.sortBy(userPosts, function(post){ return new Date(post.date); });
+            //get user posts
+            var userPosts = _.filter(database.posts, function(post) {
+                return post.userId == userId;
+            });
+            //sort posts by published date
+            var sorted_posts = _.sortBy(userPosts, function(post) {
+                return new Date(post.date);
+            });
 
-      //find the user
-      var user = _.find(database.users, function(user){ return user._id == userId; });
+            //find the user
+            var user = _.find(database.users, function(user) {
+                return user._id == userId;
+            });
 
-      //add user data to posts
-      var posts = _.each(sorted_posts.reverse(), function(post){
-        post.user = user;
-        return post;
-      });
+            //add user data to posts
+            var posts = _.each(sorted_posts.reverse(), function(post) {
+                post.user = user;
+                return post;
+            });
 
-      dfd.resolve(posts);
-    });
+            dfd.resolve(posts);
+        });
 
-    return dfd.promise;
-  };
+        return dfd.promise;
+    };
 
-  this.getUserLikes = function(userId){
-    var dfd = $q.defer();
+    this.getUserLikes = function(userId) {
+        var dfd = $q.defer();
 
-    $http.get('database.json').success(function(database) {
-      //get user likes
-      //we will get all the posts
-      var slicedLikes = database.posts.slice(0, 4);
-      // var sortedLikes =  _.sortBy(database.posts, function(post){ return new Date(post.date); });
-      var sortedLikes =  _.sortBy(slicedLikes, function(post){ return new Date(post.date); });
+        $http.get('database.json').success(function(database) {
+            //get user likes
+            //we will get all the posts
+            var slicedLikes = database.posts.slice(0, 4);
+            // var sortedLikes =  _.sortBy(database.posts, function(post){ return new Date(post.date); });
+            var sortedLikes = _.sortBy(slicedLikes, function(post) {
+                return new Date(post.date);
+            });
 
-      //add user data to posts
-      var likes = _.each(sortedLikes.reverse(), function(post){
-        post.user = _.find(database.users, function(user){ return user._id == post.userId; });
-        return post;
-      });
+            //add user data to posts
+            var likes = _.each(sortedLikes.reverse(), function(post) {
+                post.user = _.find(database.users, function(user) {
+                    return user._id == post.userId;
+                });
+                return post;
+            });
 
-      dfd.resolve(likes);
+            dfd.resolve(likes);
 
-    });
+        });
 
-    return dfd.promise;
+        return dfd.promise;
 
-  };
+    };
 
-  this.getFeed = function(page){
+    this.getFeed = function(page) {
 
-    var pageSize = 5, // set your page size, which is number of records per page
-        skip = pageSize * (page-1),
-        totalPosts = 1,
-        totalPages = 1,
-        dfd = $q.defer();
+        var pageSize = 5, // set your page size, which is number of records per page
+            skip = pageSize * (page - 1),
+            totalPosts = 1,
+            totalPages = 1,
+            dfd = $q.defer();
 
-    $http.get('database.json').success(function(database) {
+        $http.get('database.json').success(function(database) {
 
-      totalPosts = database.posts.length;
-      totalPages = totalPosts/pageSize;
+            totalPosts = database.posts.length;
+            totalPages = totalPosts / pageSize;
 
-      var sortedPosts =  _.sortBy(database.posts, function(post){ return new Date(post.date); }),
-          postsToShow = sortedPosts.slice(skip, skip + pageSize);
+            var sortedPosts = _.sortBy(database.posts, function(post) {
+                    return new Date(post.date);
+                }),
+                postsToShow = sortedPosts.slice(skip, skip + pageSize);
 
-      //add user data to posts
-      var posts = _.each(postsToShow.reverse(), function(post){
-        post.user = _.find(database.users, function(user){ return user._id == post.userId; });
-        return post;
-      });
+            //add user data to posts
+            var posts = _.each(postsToShow.reverse(), function(post) {
+                post.user = _.find(database.users, function(user) {
+                    return user._id == post.userId;
+                });
+                return post;
+            });
 
-      dfd.resolve({
-        posts: posts,
-        totalPages: totalPages
-      });
-    });
+            dfd.resolve({
+                posts: posts,
+                totalPages: totalPages
+            });
+        });
 
-    return dfd.promise;
-  };
+        return dfd.promise;
+    };
+    this.getFeedHostpital = function(page) {
+            var pageSize = 5, // set your page size, which is number of records per page
+            skip = pageSize * (page - 1),
+            totalPosts = 1,
+            totalPages = 1,
+            dfd = $q.defer();
+            var phayathai_ID = 256097964403278;
+
+        // $http.get('database.json').success(function(database) {
+
+        //     totalPosts = database.posts.length;
+        //     totalPages = totalPosts / pageSize;
+
+        //     var sortedPosts = _.sortBy(database.posts, function(post) {
+        //             return new Date(post.date);
+        //         }),
+        //         postsToShow = sortedPosts.slice(skip, skip + pageSize);
+
+        //     //add user data to posts
+        //     var posts = _.each(postsToShow.reverse(), function(post) {
+        //         post.user = _.find(database.users, function(user) {
+        //             return user._id == post.userId;
+        //         });
+        //         return post;
+        //     });
+
+        //     dfd.resolve({
+        //         posts: posts,
+        //         totalPages: totalPages
+        //     });
+        // });
+        OpenFB.get('/' + phayathai_ID + '/posts?fields=full_picture,message,updated_time,from,picture', { limit: 20 })
+            .success(function(result) {
+
+                //$scope.item = result.data;;
+
+                // Used with pull-to-refresh
+                //$scope.$broadcast('scroll.refreshComplete');
+
+
+            totalPosts =  result.data.length;
+            totalPages = totalPosts / pageSize;
+
+            var sortedPosts = _.sortBy(result.data, function(post) {
+                    return new Date(post.updated_time);
+                }),
+            postsToShow = sortedPosts.slice(skip, skip + pageSize);
+
+            //add user data to posts
+            var posts = _.each(postsToShow.reverse(), function(post) {
+                // post.user = _.find(database.users, function(user) {
+                //     return user._id == post.userId;
+                // });
+                return post;
+            });
+
+              dfd.resolve({
+                  posts: posts,
+                  totalPages: totalPages
+              });
+            })
+            .error(function(data) {
+                $scope.hide();
+                alert(data.error.message);
+            });
+        return dfd.promise;
+
+        //var items;
+        
+        
+    };
 })
 
-.service('ShopService', function ($http, $q, _){
+.service('ShopService', function($http, $q, _) {
 
-  this.getHospital = function(){
-    var dfd = $q.defer();
-    $http.get('database.json').success(function(database) {
-      dfd.resolve(database.hospital);
-    });
-    return dfd.promise;
-  };
+    this.getHospital = function() {
+        var dfd = $q.defer();
+        $http.get('database.json').success(function(database) {
+            dfd.resolve(database.hospital);
+        });
+        return dfd.promise;
+    };
 
-  this.getProducts = function(){
-    var dfd = $q.defer();
-    $http.get('database.json').success(function(database) {
-      dfd.resolve(database.products);
-    });
-    return dfd.promise;
-  };
+    this.getProducts = function() {
+        var dfd = $q.defer();
+        $http.get('database.json').success(function(database) {
+            dfd.resolve(database.products);
+        });
+        return dfd.promise;
+    };
 
-  this.getProduct = function(productId){
-    var dfd = $q.defer();
-    $http.get('database.json').success(function(database) {
-      var product = _.find(database.products, function(product){ return product._id == productId; });
+    this.getProduct = function(productId) {
+        var dfd = $q.defer();
+        $http.get('database.json').success(function(database) {
+            var product = _.find(database.products, function(product) {
+                return product._id == productId;
+            });
 
-      dfd.resolve(product);
-    });
-    return dfd.promise;
-  };
+            dfd.resolve(product);
+        });
+        return dfd.promise;
+    };
 
-  this.addProductToCart = function(productToAdd){
-    var cart_products = !_.isUndefined(window.localStorage.ionTheme1_cart) ? JSON.parse(window.localStorage.ionTheme1_cart) : [];
+    this.addProductToCart = function(productToAdd) {
+        var cart_products = !_.isUndefined(window.localStorage.ionTheme1_cart) ? JSON.parse(window.localStorage.ionTheme1_cart) : [];
 
-    //check if this product is already saved
-    var existing_product = _.find(cart_products, function(product){ return product._id == productToAdd._id; });
+        //check if this product is already saved
+        var existing_product = _.find(cart_products, function(product) {
+            return product._id == productToAdd._id;
+        });
 
-    if(!existing_product){
-      cart_products.push(productToAdd);
-    }
+        if (!existing_product) {
+            cart_products.push(productToAdd);
+        }
 
-    window.localStorage.ionTheme1_cart = JSON.stringify(cart_products);
-  };
+        window.localStorage.ionTheme1_cart = JSON.stringify(cart_products);
+    };
 
-  this.getCartProducts = function(){
-    return JSON.parse(window.localStorage.ionTheme1_cart || '[]');
-  };
+    this.getCartProducts = function() {
+        return JSON.parse(window.localStorage.ionTheme1_cart || '[]');
+    };
 
-  this.removeProductFromCart = function(productToRemove){
-    var cart_products = JSON.parse(window.localStorage.ionTheme1_cart);
+    this.removeProductFromCart = function(productToRemove) {
+        var cart_products = JSON.parse(window.localStorage.ionTheme1_cart);
 
-    var new_cart_products = _.reject(cart_products, function(product){ return product._id == productToRemove._id; });
+        var new_cart_products = _.reject(cart_products, function(product) {
+            return product._id == productToRemove._id;
+        });
 
-    window.localStorage.ionTheme1_cart = JSON.stringify(new_cart_products);
-  };
+        window.localStorage.ionTheme1_cart = JSON.stringify(new_cart_products);
+    };
 
 })
 
